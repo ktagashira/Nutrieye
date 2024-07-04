86eb3cf51fac04aaaf9fc910097cb426e92d8cda
ktagashira <ksk.tagashira@gmail.com>
2024-07-05 01:36:16 +0900

    feat: 🎸 implement flexmessage response

--- app/routes/line_callball_route.py
+++ app/routes/line_callball_route.py
@@ -1,60 +1,17 @@
 from fastapi import APIRouter, BackgroundTasks, Header, Request
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, ImageMessage
+from linebot.models import MessageEvent, TextMessage, ImageMessage, FlexSendMessage
 from starlette.exceptions import HTTPException
-from openai import OpenAI
+from pydantic import ValidationError
 
 from app import config
+from app.utils.utils import get_nutrition_from_image, create_flex_message
 
-import base64
-import mimetypes
 
 line_bot_callback_router = APIRouter(prefix="", tags=["line_bot"])
 line_bot_api = LineBotApi(config.LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(config.LINE_CHANNEL_SECRET)
-
-openai_client = OpenAI(api_key=config.OPENAI_API_KEY)
-MODEL = "gpt-4o"
-SYSTEM_PROMPT = (
-    "あなたは栄養管理をするAIです。聞かれたことに対して、JSON形式で回答します。"
-)
-USER_MESSAGE = """
-写真に写っている食べ物と、それぞれのカロリーとPFCバランスを教えてください。
-出力形式
-食べ物が写っていた場合
-{
-	"food": [
-		{
-			"name": "食べ物名",
-			"calorie": "カロリー(kcal)",
-			"protein": "タンパク質(g)",
-			"fat": "脂質(g)",
-			"carbohydrate": "炭水化物(g)"
-		}
-	],
-	"total": {
-		"calorie": "合計カロリー(kcal)",
-		"protein": "合計タンパク質(g)",
-		"fat": "合計脂質(g)",
-		"carbohydrate": "合計炭水化物(g)"
-	}
-}
-食べ物が写っていなかった場合
-{"error": "食べ物が写っていません"}
-"""
-
-
-def encode_image(image_path: str):
-    """Encodes an image to base64 and determines the correct MIME type."""
-    mime_type, _ = mimetypes.guess_type(image_path)
-    if mime_type is None:
-        raise ValueError(f"Cannot determine MIME type for {image_path}")
-
-    with open(image_path, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
-
-    return f"data:{mime_type};base64,{encoded_string}"
 
 
 @line_bot_callback_router.post("/callback")
@@ -90,35 +47,14 @@ def handle_image_massage(event):
     with open(image_file_path, "wb") as f:
         for chunk in message_content.iter_content():
             f.write(chunk)
+    try:
+        nutrition_data = get_nutrition_from_image(image_file_path)
+    except ValidationError:
+        message = TextMessage(text="食べ物が写っていません")
+        line_bot_api.reply_message(event.reply_token, message)
+        return
 
-    base64_image = encode_image(image_file_path)
-
-    response = openai_client.chat.completions.create(
-        model=MODEL,
-        response_format={"type": "json_object"},
-        messages=[
-            {
-                "role": "system",
-                "content": SYSTEM_PROMPT,
-            },
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": USER_MESSAGE,
-                    },
-                    {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": base64_image,
-                            "detail": "high",
-                        },
-                    },
-                ],
-            },
-        ],
-    )
-
-    message = TextMessage(text=response.choices[0].message.content)
+    flex_babble = create_flex_message(nutrition_data)
+    payload = flex_babble.model_dump(exclude_none=True)
+    message = FlexSendMessage(alt_text="栄養価を算出しました", contents=payload)
     line_bot_api.reply_message(event.reply_token, message)
